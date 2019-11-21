d974b9f8618f619ed791fd00ce31849aa05591ea
January <january147@outlook.com>
2019-11-21 17:40:31 +0800

    update rcp and sln

--- daily_tools/rcp.sh
+++ daily_tools/rcp.sh
@@ -3,10 +3,48 @@
 # Author: January
 
 set -o errexit
+######################config######################
+remote_dir="root@39.108.170.44:/root/store"
 
-remote_path="root@39.108.170.44:/root/store"
 
-local_path="$1"
-remote_path="$remote_path/${2#/}"
-scp "$local_path" "$remote_path" 
+function rcp_send(){
+    local_path=$1
+    remote_path="$remote_dir/${2#/}"
+    scp "$local_path" "$remote_path" 
+}
+
+function rcp_get(){
+    local_path="./$2"
+    remote_path="$remote_dir/${1#/}"
+    scp "$remote_path" "$local_path"
+}
+
+function rcp_help(){
+    echo "[usage]"
+    echo "rcp send <local_file> [<remote dir>]"
+    echo "rcp get <remote_file> [<remote dir>]"
+}
+
+while getopts "h" op
+do
+    if [ "$op" = "h" ] || [ "$op" = "?" ];then
+        rcp_help
+        exit 0
+    fi
+done
+
+action="$1"
+if [ "$action" = "send" ];then
+    rcp_send $2 $3
+elif [ "$action" = "get" ];then
+    rcp_get $2 $3
+else
+    echo "unknown action $action"
+    rcp_help
+    exit 1
+fi
+
+
+
+
 
--- daily_tools/sln.sh
+++ daily_tools/sln.sh
@@ -5,12 +5,38 @@
 # 测试是否有abspath
 abspath >> /dev/null 2>&1
 
+#################config###############
+keep_suffix=true
+################end config############
+
+function sln_help(){
+    echo "This tool is used to create soft links"
+    echo "sln <filename>"
+    echo "Note:<filename> can be multiple or wildcards"
+}
+
+while getopts "h" op
+do
+    if [ "$op" = "h" ] || [ "$op" = "?" ];then
+        sln_help
+        exit 0
+    fi
+done
+
 if [ $? -eq 0 ]; then
     while [[ $# -gt 0 ]]; do
         # 删除目录部分，留下文件名(##是从前面最大匹配后截除)
         tmp=${1##*/}
         # 删除文件后缀名部分
-        file=${tmp%.*}
+        if [ $keep_suffix = "false" ];then
+            file=${tmp%.*}
+        else
+            file=$tmp
+        fi
+        # 如果存在同名文件则改名称
+        if [ -e "$file" ];then
+            file="link_$file"
+        fi
         # 获取绝对路径，使得即使链接文件移动仍然可用
         abspath=`abspath "$1"`
         ln -s "$abspath" "$file"
